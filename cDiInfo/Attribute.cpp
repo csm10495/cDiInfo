70dbc33f8f3d532ea47ce0adfb7b28ca0f71b40b
Charles Machalow <csm10495@gmail.com>
2016-08-25 21:40:53 -0700

    Add in basic attribute class... will eventually make the AttributeMap into a (multi?)set of these, sorted by name.

---
+++ cDiInfo/Attribute.cpp
@@ -0,0 +1,206 @@
+/*
+* cDiInfo - An application to get information via the Windows SetupDi... APIs
+* Charles Machalow - MIT License - (C) 2016
+* Attribute.cpp - Implementation file for the generic attribute type
+*/
+
+// WinApi Includes
+#include <Windows.h>
+
+// STL Includes
+#include <exception>
+#include <functional>
+
+// Local Includes
+#include "Attribute.h"
+
+// #defines
+#define UNNAMED_ATTRIBUTE "Unnamed Attribute"
+#define NO_DESCRIPTION    "No Description"
+#define NO_PARSING        "No Parsing"
+
+namespace cdi
+{
+    Attribute::Attribute(BYTE* bytes, UINT64 length, std::string name, std::string description, std::string parsing)
+    {
+        if (BytesRepresentation != nullptr)
+        {
+            throw std::exception("Unable to create Attribute because the BytesRepresentation is not pre-set to nullptr");
+        }
+
+        BytesRepresentation = new BYTE[length];
+        BytesRepresentationLength = length;
+        Name = name;
+        Description = description;
+        Parsing = parsing;
+        memcpy_s(BytesRepresentation, length, bytes, length);
+    }
+
+    Attribute::Attribute(BYTE* bytes, UINT64 length) : Attribute(bytes, length, UNNAMED_ATTRIBUTE, NO_DESCRIPTION, NO_PARSING)
+    {
+
+    }
+
+    Attribute::Attribute(const Attribute &other)
+    {
+        this->BytesRepresentation = new BYTE[other.BytesRepresentationLength];
+        this->BytesRepresentationLength = other.BytesRepresentationLength;
+        this->Description = other.Description;
+        this->Name = other.Name;
+        this->Parsing = other.Parsing;
+
+        memcpy_s(this->BytesRepresentation, this->BytesRepresentationLength, other.BytesRepresentation, other.BytesRepresentationLength);
+    }
+
+    Attribute::~Attribute()
+    {
+        if (BytesRepresentation != nullptr)
+        {
+            delete[] BytesRepresentation;
+            BytesRepresentation = nullptr;
+        }
+    }
+
+    template <> char Attribute::getValue<char>()
+    {
+        checkForNullptr();
+        return *(char*)BytesRepresentation;
+    }
+
+    template <> char* Attribute::getValue<char*>()
+    {
+        checkForNullptr();
+        return (char*)BytesRepresentation;
+    }
+
+    template <> int Attribute::getValue<int>()
+    {
+        checkForNullptr();
+        return *(int*)BytesRepresentation;
+    }
+
+    template <> int* Attribute::getValue<int*>()
+    {
+        checkForNullptr();
+        return (int*)BytesRepresentation;
+    }
+
+    template <> long Attribute::getValue<long>()
+    {
+        checkForNullptr();
+        return *(long*)BytesRepresentation;
+    }
+
+    template <> long* Attribute::getValue<long*>()
+    {
+        checkForNullptr();
+        return (long*)BytesRepresentation;
+    }
+
+    template <> unsigned int Attribute::getValue<unsigned int>()
+    {
+        checkForNullptr();
+        return *(unsigned int*)BytesRepresentation;
+    }
+
+    template <> unsigned int* Attribute::getValue<unsigned int*>()
+    {
+        checkForNullptr();
+        return (unsigned int*)BytesRepresentation;
+    }
+
+    template <> unsigned long Attribute::getValue<unsigned long>()
+    {
+        checkForNullptr();
+        return *(unsigned long*)BytesRepresentation;
+    }
+
+    template <> unsigned long* Attribute::getValue<unsigned long*>()
+    {
+        checkForNullptr();
+        return (unsigned long*)BytesRepresentation;
+    }
+
+    template <> unsigned long long Attribute::getValue<unsigned long long>()
+    {
+        checkForNullptr();
+        return *(unsigned long long*)BytesRepresentation;
+    }
+
+    template <> unsigned long long* Attribute::getValue<unsigned long long*>()
+    {
+        checkForNullptr();
+        return (unsigned long long*)BytesRepresentation;
+    }
+
+    template <> long long Attribute::getValue<long long>()
+    {
+        checkForNullptr();
+        return *(long long*)BytesRepresentation;
+    }
+
+    template <> long long* Attribute::getValue<long long*>()
+    {
+        checkForNullptr();
+        return (long long*)BytesRepresentation;
+    }
+
+    template <> BYTE Attribute::getValue<BYTE>()
+    {
+        checkForNullptr();
+        return *(BYTE*)BytesRepresentation;
+    }
+
+    template <> BYTE* Attribute::getValue<BYTE*>()
+    {
+        checkForNullptr();
+        return BytesRepresentation;
+    }
+
+    template <> UINT16 Attribute::getValue<UINT16>()
+    {
+        checkForNullptr();
+        return *(UINT16*)BytesRepresentation;
+    }
+
+    template <> UINT16* Attribute::getValue<UINT16*>()
+    {
+        checkForNullptr();
+        return (UINT16*)BytesRepresentation;
+    }
+
+    template <> INT16 Attribute::getValue<INT16>()
+    {
+        checkForNullptr();
+        return *(INT16*)BytesRepresentation;
+    }
+
+    template <> std::string Attribute::getValue<std::string>()
+    {
+        return Parsing;
+    }
+
+    std::string Attribute::getName()
+    {
+        return Name;
+    }
+
+    std::string Attribute::getDescription()
+    {
+        return Description;
+    }
+
+    UINT64 Attribute::getLength()
+    {
+        return BytesRepresentationLength;
+    }
+
+    void Attribute::checkForNullptr()
+    {
+        if (BytesRepresentation == nullptr)
+        {
+            throw std::exception("BytesRepresentation is nullptr");
+        }
+    }
+
+}
---
+++ cDiInfo/Attribute.h
@@ -0,0 +1,65 @@
+/*
+* cDiInfo - An application to get information via the Windows SetupDi... APIs
+* Charles Machalow - MIT License - (C) 2016
+* Attribute.h - Header file for the generic attribute type
+*/
+#pragma once
+
+// WinApi Includes
+#include <Windows.h>
+
+// STL Includes
+#include <functional>
+
+namespace cdi
+{
+    // A generic attribute with a customizeable toString mechanism
+    class Attribute
+    {
+    public:
+        // Constructor, takes a BYTE array/length. Also takes name, description, and final parsing
+        Attribute(BYTE* bytes, UINT64 length, std::string name, std::string description, std::string parsing);
+
+        // Cheap constructor without name/description/parsing
+        Attribute(BYTE* bytes, UINT64 length);
+
+        // Copy constructor
+        Attribute(const Attribute &other);
+
+        // Destructor, deletes the internal BytesRepresentation
+        ~Attribute();
+
+        // Predefine template
+        template <class T>
+        T getValue();
+
+        // Gets the name
+        std::string getName();
+
+        // Gets the description
+        std::string getDescription();
+
+        // Gets the length of the internal byte representation
+        UINT64 getLength();
+
+    private:
+
+        // The raw bytes representation
+        BYTE* BytesRepresentation = nullptr;
+
+        // The length of the byte array
+        UINT64 BytesRepresentationLength;
+
+        // The name of the field
+        std::string Name;
+
+        // The description of the field
+        std::string Description;
+
+        // The parsing of the field
+        std::string Parsing;
+
+        // Throws an error if BytesRepresentation is nullptr
+        void checkForNullptr();
+    };
+}
